e0a9df2209f273cb1609fa9b60ef9edfb794849e
GarryChen <wojiushichenyong@126.com>
2022-04-06 21:43:51 +0800

    ecercise 3 and 4

--- kern/pmap.c
+++ kern/pmap.c
@@ -277,6 +277,13 @@ mem_init_mp(void)
 	//
 	// LAB 4: Your code here:
 
+	for (int i=0; i<NCPU; i++) {
+		uint32_t kstacktop_i = KSTACKTOP - i * (KSTKSIZE + KSTKGAP);
+		
+		boot_map_region(kern_pgdir, kstacktop_i - KSTKSIZE, 
+						KSTKSIZE, PADDR(percpu_kstacks[i]), PTE_W);
+	}
+
 }
 
 // --------------------------------------------------------------
