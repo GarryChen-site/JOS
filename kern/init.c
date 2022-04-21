7dae01af0ade204e96e0ef213811a5489dc0a49e
GarryChen <wojiushichenyong@126.com>
2022-04-21 20:28:26 +0800

    roll back some "cprintf", lab4 final pass

--- kern/init.c
+++ kern/init.c
@@ -56,7 +56,7 @@ i386_init(void)
 	ENV_CREATE(TEST, ENV_TYPE_USER);
 #else
 	// Touch all you want.
-	ENV_CREATE(user_icode, ENV_TYPE_USER);
+	// ENV_CREATE(user_icode, ENV_TYPE_USER);
 	// create three environments 
 	// why this works is in lab3
 	ENV_CREATE(user_yield, ENV_TYPE_USER);
