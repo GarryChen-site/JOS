0449198a6bc36c762a69e884cbf231be55f1ba36
GarryChen <wojiushichenyong@126.com>
2022-04-21 21:31:29 +0800

    fs i/o padd

--- kern/env.c
+++ kern/env.c
@@ -435,6 +435,10 @@ env_create(uint8_t *binary, enum EnvType type)
 
 	load_icode(env, binary);
 	env->env_type = type;
+
+	if (type == ENV_TYPE_FS) {
+		env->env_tf.tf_eflags |= FL_IOPL_MASK;
+	}
 }
 
 //
