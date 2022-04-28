5f166d1bd8613c0a5160e0f14eb6b328b065bebb
GarryChen <wojiushichenyong@126.com>
2022-04-28 22:47:20 +0800

    Exercise 4 pass

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,4 +1,36 @@
 #include <kern/e1000.h>
 #include <kern/pmap.h>
+#include <inc/string.h>
 
 // LAB 6: Your driver code here
+
+volatile void *e1000_mmio;
+#define E1000REG(offset)  (void *)(e1000_mmio+offset)
+
+
+static struct e1000_desc e1000_tx_queue[NTXDESC] __attribute__((aligned(16)));
+static uint8_t e1000_tx_buf[NTXDESC][TX_BUF_SIZE];
+
+
+int pci_e1000_attach(struct pci_func *pcif){
+  pci_func_enable(pcif);
+
+  e1000_mmio = (void *)mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+  cprintf("PCI E1000 status is 0x%x\n", *(uint32_t *)E1000REG(E1000_STATUS));
+  return 1;
+}   
+
+
+void e1000_transmit_init() {
+
+  // init tx queue
+  int i;
+
+  // memset(e1000_tx_queue,0, sizeof(e1000_tx_queue));
+
+  for (i=0; i<NTXDESC; i++){
+    // e1000_tx_queue[i].addr = PADDR(e1000_tx_buf[i]);
+  }
+
+
+}
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,3 +1,68 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
 #endif  // SOL >= 6
+
+#include <kern/pci.h>
+
+#define E1000_STATUS   0x00008  /* Device Status - RO */
+
+int pci_e1000_attach(struct pci_func *pcif);
+void e1000_transmit_init();
+
+/* transmit queue */
+#define TX_BUF_SIZE 1536  // 16-byte aligned for performance
+#define NTXDESC     64
+
+
+
+
+
+/* PCI Vendor ID */
+#define E1000_VENDOR_ID_82540EM 0x8086
+/* PCI Device IDs */
+#define E1000_DEV_ID_82540EM  0x100E
+
+#define E1000_TCTL     0x00400  /* TX Control - RW */
+#define E1000_TIPG     0x00410  /* TX Inter-packet gap -RW */
+#define E1000_TDBAL    0x03800  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH    0x03804  /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN    0x03808  /* TX Descriptor Length - RW */
+#define E1000_TDH      0x03810  /* TX Descriptor Head - RW */
+#define E1000_TDT      0x03818  /* TX Descripotr Tail - RW */
+
+/* Transmit Control */
+#define E1000_TCTL_EN   0x00000002  /* enable tx */
+#define E1000_TCTL_PSP  0x00000008  /* pad short packets */
+#define E1000_TCTL_CT   0x00000ff0  /* collision threshold */
+#define E1000_TCTL_COLD 0x003ff000  /* collision distance */
+
+/* Collision related configuration parameters */
+#define E1000_COLLISION_THRESHOLD   0x10
+
+/* Collision distance is a 0-based value that applies to half-duplex-capable hardware only. */
+#define E1000_COLLISION_DISTANCE    0x40
+
+/* 13.43 */
+/* Default values for the transmit IPG register */
+#define E1000_DEFAULT_TIPG_IPGT     10
+#define E1000_DEFAULT_TIPG_IPGR1    4
+#define E1000_DEFAULT_TIPG_IPGR2    6
+
+// transmit descriptor
+struct e1000_tdesc{
+  uint64_t addr;
+  uint16_t length;
+  uint8_t cso;
+  uint8_t cmd;
+  uint8_t status;
+  uint8_t css;
+  uint16_t special;
+}__attribute__((packed));
+
+
+
+
+
+
+
+
